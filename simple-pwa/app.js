0e0aab6cb3f5bbb12ab327a4d125f2b0a077d85f
Shrushiv Telang <shrushiv.telang@education.nsw.gov.au>
2026-03-16 14:25:40 +1100

    Update app.js

--- simple-pwa/app.js
+++ simple-pwa/app.js
@@ -1,1 +1,6 @@
-
+// Register the service worker
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/web_dev/simple-pwa/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg))
+      .catch(err => console.log('Service Worker registration failed:', err));
+  }
